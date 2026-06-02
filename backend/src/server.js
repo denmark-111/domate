5ddd0f17fe577f5009986d581f28f71915035ba5
Denmarc Maglipon <maglipon.denmarc.amar@gmail.com>
2026-06-02 19:05:46 +0800

    chore(backend): setup prisma and initial workspace schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+import express from "express";
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
